db7bbeae282fe5778849e1cb58ea6e080524db11
Matthew Hartley <mhartley@cantab.net>
2017-04-27 16:22:26 +0100

    Initial segmentation

--- run-development-container.sh
+++ run-development-container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 CONTAINER="seed_cell_size_2d-development"
+DATADIR=/Users/hartleym/data_repo
 touch `pwd`/bash_history
-docker run -it --rm -v `pwd`/bash_history:/root/.bash_history -v `pwd`/data:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
+docker run -it --rm -v `pwd`/bash_history:/root/.bash_history -v $DATADIR:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
--- scripts/analysis.py
+++ scripts/analysis.py
@@ -4,9 +4,24 @@ import os
 import logging
 import argparse
 
+import dtool
+
+import numpy as np
+
+from scipy.misc import imsave
+
+from skimage.filters import threshold_adaptive
+
 from jicbioimage.core.image import Image
 from jicbioimage.core.transform import transformation
 from jicbioimage.core.io import AutoName, AutoWrite
+from jicbioimage.segment import connected_components
+
+from jicbioimage.transform import (
+    invert,
+    threshold_otsu,
+    remove_small_objects
+)
 
 __version__ = "0.0.1"
 
@@ -19,29 +34,55 @@ def identity(image):
     return image
 
 
+def preprocess_and_segment(image):
+
+    thresholded = threshold_adaptive(image, block_size=91)
+
+    image = remove_small_objects(thresholded)
+    image = invert(image)
+    image = remove_small_objects(image)
+    image = invert(image)
+
+    segmentation = connected_components(image, background=False)
+
+    return segmentation
+
+
 def analyse_file(fpath, output_directory):
     """Analyse a single file."""
     logging.info("Analysing file: {}".format(fpath))
     image = Image.from_file(fpath)
-    image = identity(image)
+    print("analysing {}".format(fpath))
+    print(image.shape)
+
+    segmentation = preprocess_and_segment(image)
+
+    print(segmentation.identifiers)
 
 
-def analyse_directory(input_directory, output_directory):
-    """Analyse all the files in a directory."""
-    logging.info("Analysing files in directory: {}".format(input_directory))
-    for fname in os.listdir(input_directory):
-        fpath = os.path.join(input_directory, fname)
-        analyse_file(fpath, output_directory)
+def analyse_dataset(dataset_dir, output_dir):
+    """Analyse all the files in the dataset."""
+    dataset = dtool.DataSet.from_path(dataset_dir)
+    logging.info("Analysing files in dataset: {}".format(dataset.name))
+
+    i = dataset.identifiers[0]
+
+    rel_path = dataset.item_path_from_hash(i)
+
+    analyse_file(rel_path, output_dir)
 
 
 def main():
     # Parse the command line arguments.
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("input_source", help="Input file/directory")
+    parser.add_argument("dataset_dir", help="Input dataset directory")
     parser.add_argument("output_dir", help="Output directory")
     parser.add_argument("--debug", default=False, action="store_true",
                         help="Write out intermediate images")
     args = parser.parse_args()
+
+    if not os.path.isdir(args.dataset_dir):
+        parser.error("Not a directory: {}".format(args.dataset_dir))
 
     # Create the output directory if it does not exist.
     if not os.path.isdir(args.output_dir):
@@ -64,13 +105,7 @@ def main():
     logging.info("Script name: {}".format(__file__))
     logging.info("Script version: {}".format(__version__))
 
-    # Run the analysis.
-    if os.path.isfile(args.input_source):
-        analyse_file(args.input_source, args.output_dir)
-    elif os.path.isdir(args.input_source):
-        analyse_directory(args.input_source, args.output_dir)
-    else:
-        parser.error("{} not a file or directory".format(args.input_source))
+    analyse_dataset(args.dataset_dir, args.output_dir)
 
 if __name__ == "__main__":
     main()
