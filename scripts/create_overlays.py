5ed259f310935dc47d64aaa26b37a38a5593ba03
Matthew Hartley <mhartley@cantab.net>
2017-10-10 12:17:32 +0100

    Update tools

--- scripts/create_overlays.py
+++ scripts/create_overlays.py
@@ -4,13 +4,12 @@ import os
 
 import click
 
-from dtool_azure import AzureDataSet
-
+from dtoolcore import DataSet
 
 def create_useful_name_overlay(dataset):
 
     def name_from_identifier(identifier):
-        path = dataset.item_properties(identifier)['path']
+        path = dataset.item_properties(identifier)['relpath']
         return path.rsplit('.', 1)[0]
 
     useful_name_overlay = {
@@ -22,24 +21,23 @@ def create_useful_name_overlay(dataset):
 
 
 @click.command()
-@click.argument('uuid')
-@click.option('--config-path', type=click.Path(exists=True))
-def main(uuid, config_path=None):
+@click.argument('uri')
+def main(uri):
 
-    dataset = AzureDataSet.from_uri(uuid, config_path=config_path)
-    manifest = dataset._manifest
+    dataset = DataSet.from_uri(uri)
+    # manifest = dataset._manifest
 
-    def name_from_identifier(identifier):
-        path = manifest["items"][identifier]['path']
-        return path.rsplit('.', 1)[0]
+    # def name_from_identifier(identifier):
+    #     path = manifest["items"][identifier]['path']
+    #     return path.rsplit('.', 1)[0]
 
-    useful_name_overlay = {
-        identifier: name_from_identifier(identifier)
-        for identifier in dataset.identifiers
-    }
+    # useful_name_overlay = {
+    #     identifier: name_from_identifier(identifier)
+    #     for identifier in dataset.identifiers
+    # }
 
-    dataset.put_overlay("useful_name", useful_name_overlay)
-    # create_useful_name_overlay(dataset)
+    # dataset.put_overlay("useful_name", useful_name_overlay)
+    create_useful_name_overlay(dataset)
 
 if __name__ == '__main__':
     main()
--- scripts/seedcellsize.py
+++ scripts/seedcellsize.py
@@ -1,13 +1,72 @@
 """Wrapper script for seed cell size analysis."""
 
-from smarttool import SmartTool
+import os
+import argparse
+import subprocess
+
+from dtoolcore import DataSet, ProtoDataSet
+
+from dtoolutils import temp_working_dir, stage_outputs
+
+
+class PythonSmartTool(object):
+
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            '-d',
+            '--dataset',
+            help='URI of input dataset'
+        )
+        parser.add_argument(
+            '-i',
+            '--identifier',
+            help='Identifier (hash) to process'
+        )
+        parser.add_argument(
+            '-o',
+            '--output-dataset',
+            help='URI of output dataset'
+        )
+
+        args = parser.parse_args()
+
+        self.input_dataset = DataSet.from_uri(args.dataset)
+        self.output_dataset = ProtoDataSet.from_uri(args.output_dataset)
+
+        self.identifier = args.identifier
+
+    def run(self):
+
+        input_path = self.input_dataset.item_content_abspath(self.identifier)
+
+        with temp_working_dir() as tmpdir:
+            command = ["python", "/scripts/analysis.py"]
+            command += ["-i", input_path]
+            command += ["-o", tmpdir]
+
+            subprocess.call(command)
+
+            outputs_with_metadata = [(o, {}) for o in self.outputs]
+
+            stage_outputs(
+                outputs_with_metadata,
+                tmpdir,
+                self.input_dataset,
+                self.output_dataset,
+                [],
+                self.identifier
+            )
 
 
 def main():
 
-    tool = SmartTool()
-    tool.container = "jicscicomp/seedcellsize"
-    tool.command_string = "python /scripts/analysis.py -i /input1 -o /output"
+    tool = PythonSmartTool()
+
+    # tool.run()
+    # tool.container = "jicscicomp/seedcellsize"
+    # tool.command_string = "python /scripts/analysis.py -i /input1 -o /output"
     tool.outputs = [
         'original.png',
         'segmentation.png',
